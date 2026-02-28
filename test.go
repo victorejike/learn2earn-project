e702c5ab11f03e16b35f5d562215b1261b8d93b3
victor ejike <victorejiki@gmail.com>
2026-02-28 12:29:37 +0000

    about print functions

---
+++ test.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	//the hole aim of this is to test and see the diffrent use of print function
+	var j, k string = "hello", "world!"
+	print(j, "\n")
+	print(k, "\n")
+}
