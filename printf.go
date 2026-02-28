984783ae699d0903dfc51df1fb7feaad150f7fa0
victor ejike <victorejiki@gmail.com>
2026-02-28 12:53:57 +0000

    function printf

---
+++ printf.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var j string = "hello"
+	var k int = 15
+	fmt.Printf("j has the value of: %V and type: %T\n", j, j)
+	fmt.Printf("k has the value of : %V and type: %T\n", k, k)
+}
