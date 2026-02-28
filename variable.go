88c174aa96bdd3bc94c7be0fad81058a9dce42ea
victor ejike <victorejiki@gmail.com>
2026-02-28 12:12:52 +0000

    the implematation of variable

---
+++ variable.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	//what this does is that it keeps the variable to a const so that it can be reused
+	const PI = 30.37
+	// this to print out the out put
+	fmt.Println(PI)
+}
